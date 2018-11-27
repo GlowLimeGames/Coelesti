9f3bc330655db376f6c1fd56f493e3397aa15073
Isaiah Mann <isaiahbmann@gmail.com>
2018-11-26 20:51:08 -0800

    Add basic character movement (#6)

---
+++ Assets/Scripts/Gameplay/Camera/FollowCamera.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class FollowCamera : MonoBehaviour 
+{
+    [SerializeField]
+    Transform followTarget;
+
+    Vector3 previousPosition = Vector3.negativeInfinity;
+
+	// Update is called once per frame
+	void FixedUpdate() 
+    {
+        if(followTarget)
+        {
+            if(!previousPosition.Equals(Vector3.negativeInfinity))
+            {
+                transform.position += followTarget.position - previousPosition;
+            }
+            previousPosition = followTarget.position;
+        }
+	}
+
+    public void Follow(Transform followTarget)
+    {
+        this.followTarget = followTarget;
+    }
+}
---
+++ Assets/Scripts/Gameplay/Navigation/CharacterMovementController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class CharacterMovementController : MonoBehaviour 
+{
+    [SerializeField]
+    KeyCode moveUpKey, moveDownKey, moveLeftKey, moveRightKey;
+
+    [SerializeField]
+    float speed = 1f;
+
+	// Update is called once per frame
+	void FixedUpdate() 
+    {
+        if(Input.GetKey(moveUpKey)) 
+        {
+            transform.position += Vector3.up * speed;
+        }
+        if(Input.GetKey(moveDownKey))
+        {
+            transform.position += Vector3.down * speed;
+        }
+        if(Input.GetKey(moveLeftKey))
+        {
+            transform.position += Vector3.left * speed;
+        }
+        if(Input.GetKey(moveRightKey))
+        {
+            transform.position += Vector3.right * speed;
+        }
+    }
+}
