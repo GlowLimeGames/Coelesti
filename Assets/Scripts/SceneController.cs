eda506a0356bbb72bb81f2425a1e55bcd896f2cd
Isaiah Mann <contact@isaiahmann.com>
2018-12-03 19:11:35 -0800

    Add example of loading a new scene

---
+++ Assets/Scripts/SceneController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneController : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+    
+    public void LoadScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
