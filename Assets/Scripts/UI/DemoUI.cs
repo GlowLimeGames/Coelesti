f7f1df0fd340c0a54a435fac78c242eb2c98704d
Isaiah Mann <contact@isaiahmann.com>
2018-12-10 19:13:07 -0800

    Button use demo

---
+++ Assets/Scripts/UI/DemoUI.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DemoUI : MonoBehaviour {
+    public void LogLines(string message) 
+    {
+        Debug.Log("Hello World");
+        Debug.Log("Foo Bar");
+        Debug.LogWarning("Warned You");
+        Debug.LogFormat("My custom message: {0}", message);
+    }
+}
